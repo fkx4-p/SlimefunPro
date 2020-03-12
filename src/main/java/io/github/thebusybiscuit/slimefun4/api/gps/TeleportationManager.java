ec8bc13588d5ada67efdcd7c21e060b18cc6bab6
FFEE_CO <renjianji20040415@gmail.com>
2020-03-12 11:35:10 +0800

    Update TeleportationManager.java

--- src/main/java/io/github/thebusybiscuit/slimefun4/api/gps/TeleportationManager.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/api/gps/TeleportationManager.java
@@ -53,7 +53,8 @@ public final class TeleportationManager {
         menu.addItem(4, new CustomItem(SkullItem.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzljODg4MWU0MjkxNWE5ZDI5YmI2MWExNmZiMjZkMDU5OTEzMjA0ZDI2NWRmNWI0MzliM2Q3OTJhY2Q1NiJ9fX0="), "&7Waypoint Overview &e(Select a Destination)"));
         menu.addMenuClickHandler(4, ChestMenuUtils.getEmptyClickHandler());
 
-        Location source = new Location(b.getWorld(), b.getX() + 0.5D, b.getY() + 2D, b.getZ() + 0.5D);
+        Location source = new Location(b.getWorld(), b.getX(), b.getY(), b.getZ());
+        //Location source = new Location(b.getWorld(), b.getX() + 0.5D, b.getY() + 2D, b.getZ() + 0.5D);
         int index = 0;
 
         for (Map.Entry<String, Location> entry : network.getWaypoints(uuid).entrySet()) {
@@ -89,12 +90,17 @@ public final class TeleportationManager {
     public int getSpeed(int complexity, Location source, Location destination) {
         int speed = complexity;
         if(!(source.getWorld().getName().equals(destination.getWorld().getName()))) speed *= 0.1;
+
         speed = ((complexity < distance(source, destination)) ? ((complexity - 5 * (distance(source, destination) - complexity)) / 200) : speed / 200);
-        if(complexity <= 20000) return speed < 1 ? 1 : speed;
+
+        if(complexity == 0) return -1;
+        /*else if(complexity <= 20000) return speed < 1 ? 1 : speed;
         else if(complexity <= 50000) return speed < 5 ? 5 : speed;
         else if(complexity <= 80000) return speed < 10 ? 10 : speed;
         else if(complexity <= 100000) return speed < 25 ? 25 : speed;
+        else if(complexity > 100000) return speed < 35 ? 35 : speed;*/
         /********** If you want to get fucked, enable the above code. **********/
+        
         // if (speed > 1500000000) speed = 1500000000;//max speed changed from 50%/sec -> 500%/s(now that 10w complexity is "full speed")
         // if (!(source.getWorld().getName().equals(destination.getWorld().getName())))
         //     return (int) (speed / 10);
@@ -127,6 +133,14 @@ public final class TeleportationManager {
         return p != null && p.getLocation().distanceSquared(source) < 2.0;
     }
 
+    private void no_complexity(UUID uuid, Player p){
+        teleporterUsers.remove(uuid);
+
+        if (p != null) {
+            p.sendTitle(ChatColors.color(SlimefunPlugin.getLocal().getMessage(p, "machines.TELEPORTER.cancelled")), ChatColors.color("You don't have any satellites!"), 20, 60, 20);
+        }
+    }
+
     private void cancel(UUID uuid, Player p) {
         teleporterUsers.remove(uuid);
 
@@ -138,7 +152,11 @@ public final class TeleportationManager {
     private void updateProgress(UUID uuid, int speed, int progress, Location source, Location destination, boolean resistance) {
         Player p = Bukkit.getPlayer(uuid);
 
-        if (isValid(p, source)) {
+        if(speed == -1){
+            no_complexity(uuid, p);
+        }
+
+        else if (isValid(p, source)) {
             if (progress > 99) {
                 p.sendTitle(ChatColors.color(SlimefunPlugin.getLocal().getMessage(p, "machines.TELEPORTER.teleported")), ChatColors.color("&b100%"), 20, 60, 20);
 
