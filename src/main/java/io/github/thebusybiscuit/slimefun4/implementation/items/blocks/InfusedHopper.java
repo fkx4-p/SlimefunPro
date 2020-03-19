8db05b72888b10bd85f8ba0d63f05708ea87182d
ishland <ishlandmc@yeah.net>
2020-03-20 07:56:23 +0800

    Minor change to InfusedHopper

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/blocks/InfusedHopper.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/blocks/InfusedHopper.java
@@ -1,14 +1,5 @@
 package io.github.thebusybiscuit.slimefun4.implementation.items.blocks;
 
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Item;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Vector;
-
 import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
@@ -17,6 +8,14 @@ import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.Objects.handlers.BlockTicker;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 public class InfusedHopper extends SimpleSlimefunItem<BlockTicker> {
 
@@ -38,7 +37,7 @@ public class InfusedHopper extends SimpleSlimefunItem<BlockTicker> {
                     return;
                 }
 
-                Location l = b.getLocation().add(0.5, 1.2, 0.5);
+                Location l = b.getLocation().add(0.5, 0.8, 0.5);
                 boolean sound = false;
 
                 for (Entity item : b.getWorld().getNearbyEntities(l, 3.5D, 3.5D, 3.5D, n -> n instanceof Item && n.isValid() && !n.hasMetadata("no_pickup") && n.getLocation().distanceSquared(l) > 0.25)) {
