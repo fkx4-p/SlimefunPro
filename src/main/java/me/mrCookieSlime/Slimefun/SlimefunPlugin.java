f3586711f6cb3bc1ae44a44d75acffcc8870c104
ishland <ishlandmc@yeah.net>
2020-03-24 11:01:59 +0800

    Field change

--- src/main/java/me/mrCookieSlime/Slimefun/SlimefunPlugin.java
+++ src/main/java/me/mrCookieSlime/Slimefun/SlimefunPlugin.java
@@ -321,9 +321,10 @@ public final class SlimefunPlugin extends JavaPlugin implements SlimefunAddon {
                         final Field consoleField = Bukkit.getServer().getClass().getDeclaredField("console");
                         consoleField.setAccessible(true);
                         Object DedicatedServer = consoleField.get(Bukkit.getServer());
-                        final Field hasStoppedField = DedicatedServer.getClass().getDeclaredField("hasStopped");
-                        hasStoppedField.setAccessible(true);
-                        Slimefun.isStopping = hasStoppedField.getBoolean(DedicatedServer);
+                        final Field isRunningField = DedicatedServer.getClass()
+                                .getSuperclass().getDeclaredField("isRunning");
+                        isRunningField.setAccessible(true);
+                        Slimefun.isStopping = !isRunningField.getBoolean(DedicatedServer);
                         Slimefun.getLogger().warning(String.valueOf(Slimefun.isStopping));
                     } catch (IllegalAccessException | NoSuchFieldException e) {
                         Slimefun.getLogger().log(Level.WARNING, "Error while fetching state", e);
